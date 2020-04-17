9c8a693747b7e98f6faeb8006926a47e60d96863
Eduardo Renani <eduardo.renani@hotmail.com>
2020-04-17 00:03:10 -0300

    viewmodel ingredients done

---
+++ Drinks SwiftUI/Screens/Ingredients/DrinksFromIngredient/DrinksFromIngredientViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DrinksFromIngredientViewModel.swift
+//  Drinks SwiftUI
+//
+//  Created by Eduardo Ribeiro on 16/04/20.
+//  Copyright © 2020 Rodrigo Giglio. All rights reserved.
+//
+
+import Foundation
